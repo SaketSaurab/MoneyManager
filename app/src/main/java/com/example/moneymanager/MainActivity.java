c7ed0c148b45820abe87280f0948b6c88edb5b43
SaketSaurab <100rabhsinghalld@gmail.com>
2021-07-21 14:41:12 +0530

    error

---
+++ app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.moneymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    button=(Button) findViewById(R.id.btncontinue);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            intent = new Intent(MainActivity.this,SignInActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    }
+
+}
