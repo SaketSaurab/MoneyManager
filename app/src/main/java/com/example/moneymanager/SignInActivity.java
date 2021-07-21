1c8fe0e47d07bc71c9dc3b2c844b7a9334dbf151
SaketSaurab <100rabhsinghalld@gmail.com>
2021-07-21 14:58:39 +0530

    error 2

--- app/src/main/java/com/example/moneymanager/SignInActivity.java
+++ app/src/main/java/com/example/moneymanager/SignInActivity.java
@@ -8,20 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
-    public Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        button=(Button) findViewById(R.id.btn_register);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(SignInActivity.this,SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        };
     }
-}
