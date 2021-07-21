ea8219c686caf2dec998e5ac93f4fe6a4e3d49b5
SaketSaurab <100rabhsinghalld@gmail.com>
2021-07-21 15:40:09 +0530

    change 3

--- app/src/main/java/com/example/moneymanager/SignInActivity.java
+++ app/src/main/java/com/example/moneymanager/SignInActivity.java
@@ -8,13 +8,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
-
+Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+button1=(Button) findViewById(R.id.createnow);
+button1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent1=new Intent(SignInActivity.this,SignUpActivity.class);
+        startActivity(intent1);
+    }
+});
 
         };
     }
