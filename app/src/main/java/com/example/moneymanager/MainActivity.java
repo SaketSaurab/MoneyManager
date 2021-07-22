9f1028a1ad13b8be7d25140612c2f4e4f702641e
SaketSaurab <100rabhsinghalld@gmail.com>
2021-07-22 07:58:50 +0530

    Splash Screen

--- app/src/main/java/com/example/moneymanager/MainActivity.java
+++ app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -4,26 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    button=(Button) findViewById(R.id.btncontinue);
-    button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent;
-            intent = new Intent(MainActivity.this,SignInActivity.class);
-            startActivity(intent);
-        }
-    });
-
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent i = new Intent(MainActivity.this, SignInActivity.class); startActivity(i);
+                finish(); } }, 2000);
     }
 
-}
+        }
